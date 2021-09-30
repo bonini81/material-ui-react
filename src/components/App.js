da51b8f57b7da696b035b1d9fc253db76521c6e7
Dominguez Bonini Andrés Santiago <andres.dominguez@novaseguroslatam.com>
2021-09-29 21:59:14 -0500

    Change on App.js page title

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
+import {BrowserRouter, Route, Switch} from "react-router-dom";     
 import theme from './ui/Theme';
 import Header from '../components/ui/Header';
 
@@ -7,8 +8,21 @@ import Header from '../components/ui/Header';
 function App() {
   return (
     <ThemeProvider theme={theme} >
+      <BrowserRouter>
       <Header />
-     Hello!
+      <Switch>
+        <Route exact path="/" component={()=> <div>Home 2</div> }></Route>
+        <Route exact path="/services" component={()=> <div>Services</div> }></Route>
+        <Route exact path="/customsoftware" component={()=> <div>Custom Software</div> }></Route>
+        <Route exact path="/mobileapps" component={()=> <div>Mobile Apps</div> }></Route>
+        <Route exact path="/websites" component={()=> <div>Websites</div> }></Route>
+        <Route exact path="/revolution" component={()=> <div>Revolution</div> }></Route>
+        <Route exact path="/about" component={()=> <div>About</div> }></Route>
+        <Route exact path="/contact" component={()=> <div>Contact</div> }></Route>
+        <Route exact path="/estimate" component={()=> <div>Estimate</div> }></Route>
+      </Switch>
+      </BrowserRouter>
+
     </ThemeProvider>
   );
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles } from '@material-ui/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
-
+import {Link} from "react-router-dom";
 
 import logo from '../../assets/logo.svg';
 
@@ -17,6 +17,9 @@ const useStyles = makeStyles(theme => ({
     toolbarMargin: {
         ...theme.mixins.toolbar,
         marginBottom: "3em" 
+    },
+    logoContainer: {
+        padding: 0
     },
     logo: {
         height: "5em"
@@ -46,23 +49,42 @@ export default function Header(props){
         setValue(value)
 }
 
+    useEffect(() => {
+        if (window.location.pathname === "/" && value !== 0 ) {
+            setValue(0)
+        } else if (window.location.pathname === "/services" && value !== 1 ) {
+            setValue(1)
+        } else if (window.location.pathname === "/revolution" && value !== 2 ) {
+            setValue(2)
+        }  else if (window.location.pathname === "/about" && value !== 3 ) {
+            setValue(3)
+        }  else if (window.location.pathname === "/contact" && value !== 4 ) {
+            setValue(4)
+        }  else if (window.location.pathname === "/estimate" && value !== 5 ) {
+            setValue(5)
+        }
+        
+    }, [value])
+
 return (
 /** 
 * Disable Gutters is for taking out all paddings and margin from a container (Ej. <div>)
 */
 <React.Fragment>
 <AppBar position="fixed" color="primary">
- 
+  
 <Toolbar disableGutters>
+    <Button component={Link} to="/" className={classes.logoContainer} onClick={ ()=> setValue(0) }>
 <img src={logo} alt="Company Logo" className={classes.logo} /> 
+    </Button>
 <Tabs value={value}  onChange={handleChange} className={classes.tabContainer} indicatorColor="primary"  > 
-    <Tab className={classes.tab} label="Home" />
-    <Tab className={classes.tab} label="Services" />
-    <Tab className={classes.tab} label="The Revolution" />
-    <Tab className={classes.tab} label="About Us" />
-    <Tab className={classes.tab} label="Contact Us" />
+    <Tab className={classes.tab} label="Home" component={Link} to="/"  />
+    <Tab className={classes.tab} label="Services" component={Link} to="/services" />
+    <Tab className={classes.tab} label="The Revolution" component={Link} to="/revolution" />
+    <Tab className={classes.tab} label="About Us" component={Link} to="/about" />
+    <Tab className={classes.tab} label="Contact Us" component={Link} to="/contact" />
 </Tabs>
-<Button variant="contained" color="secondary" className={classes.button}>Free Estimate</Button>
+<Button variant="contained" color="secondary" className={classes.button} to="/contact" >Free Estimate</Button>
 </Toolbar>
 </AppBar>
 <div className={classes.toolbarMargin} />
